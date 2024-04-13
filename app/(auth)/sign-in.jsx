8456701fcb4655932a81a50d196832d9cc6cebaf
Shreyas <shreyas.mohanty03@gmail.com>
2024-04-13 13:02:56 +0530

    signin updated

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,64 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from "react-native";
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButtom from "../../components/CustomButton";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {};
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
+    <SafeAreaView className=" bg-primary h-full">
+      <ScrollView bounces>
+        <View className=" w-full justify-center h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className=" w-[115px] h-[35px]"
+          />
+          <Text className=" text-2xl text-white font-semibold mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) =>
+              setForm({
+                ...form,
+                email: e,
+              })
+            }
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) =>
+              setForm({
+                ...form,
+                password: e,
+              })
+            }
+            otherStyles="mt-7"
+          />
+          <CustomButtom
+            title={"Sign In"}
+            handlePress={submit}
+            containerStyles={"mt-7"}
+            isLoading={isSubmitting}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
