146666fa34e37afa30b63a83df75e6c9c9cf840a
Shreyas <shreyas.mohanty03@gmail.com>
2024-04-13 11:30:50 +0530

    Refactor app/index.jsx to use new components and styles

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Image, ScrollView, Text, View } from "react-native";
+import { router } from "expo-router";
 import { NativeWindStyleSheet } from "nativewind";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
@@ -9,11 +12,44 @@ NativeWindStyleSheet.setOutput({
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Link href={"/home"} style={{ fontFamily: "Poppins-Bold" }}>
-        Go to Home
-      </Link>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className=" bg-[#161622] h-full">
+      <ScrollView bounces contentContainerStyle={{ height: "100.1%" }}>
+        <View className=" w-full justify-center items-center min-h-[85vh] px-4">
+          <Image
+            source={images.logo}
+            className=" w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className=" max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className=" relative mt-5">
+            <Text className=" text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200 text-3xl font-pbold text-center">
+                Aora
+              </Text>
+            </Text>
+            <Image
+              source={images.path}
+              className=" w-[96px] h-[15px] absolute -bottom-2 -right-[-110px] "
+              resizeMode="contain"
+            />
+          </View>
+          <Text className=" text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aora
+          </Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles=" w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
